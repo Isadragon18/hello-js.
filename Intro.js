6e944bd49809ccddbe15a662f47b5f30822dbb68
Yusuf Isaiah Ifeanyichukwu <isawhizi@gmail.com>
2026-05-13 20:22:29 +0100

    My Assignemnt for the First Lesson

--- Intro.js
+++ Intro.js
@@ -1,1 +1,5 @@
-console.log("Isaiah");
+const name = "Yusuf Isaiah Ifeanyichukwu";
+const age = 27;
+const program = "Python"
+
+console.log("My name is " + name + ", I am " + age + "years old. I love " + program + " Language.");
