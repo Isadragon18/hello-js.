c585ba88ee7fe44f06c95e3a907c726dec107d6b
Yusuf Isaiah Ifeanyichukwu <isawhizi@gmail.com>
2026-05-16 19:58:56 +0100

    My First Project

--- Intro.js
+++ Intro.js
@@ -1,6 +1,5 @@
-const name = "Yusuf Isaiah Ifeanyichukwu";
-const age = 27;
+const name = "Yusuf Isaiah Ifeanyichukwu"
+const age = 27
 const program = "Python"
-let name = "Isaiah"
 
-console.log("My name is " + name + ", I am " + age + "years old. I love " + program + " Language.");
+console.log("My name is " + name + ", I am " + age + "years old. I love " + program + " Language.")
