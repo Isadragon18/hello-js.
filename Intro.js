befc82aa019de3effa09e0e2c0e1f5066d168a3e
Yusuf Isaiah Ifeanyichukwu <isawhizi@gmail.com>
2026-05-16 19:42:04 +0100

    Testing Push

--- Intro.js
+++ Intro.js
@@ -1,5 +1,6 @@
 const name = "Yusuf Isaiah Ifeanyichukwu";
 const age = 27;
 const program = "Python"
+let name = "Isaiah"
 
 console.log("My name is " + name + ", I am " + age + "years old. I love " + program + " Language.");
