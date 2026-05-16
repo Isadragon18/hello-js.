486ca7853caeb066f947478ed47cb5f37641a594
Yusuf Isaiah Ifeanyichukwu <isawhizi@gmail.com>
2026-05-16 20:18:21 +0100

    Using Format method for My output

--- Intro.js
+++ Intro.js
@@ -1,5 +1,6 @@
-const name = "Yusuf Isaiah Ifeanyichukwu"
-const age = 27
-const program = "Python"
+const name = "Yusuf Isaiah Ifeanyichukwu";
+const age = 27;
+const program = "Python";
 
-console.log("My name is " + name + ", I am " + age + "years old. I love " + program + " Language.")
+console.log(`My name is ${name}, I am ${age} years old. I love ${program} Language.`);
+console.log(`Hello ${name},`);
